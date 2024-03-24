8c5cb9c00d1e7b4dd3c84bd733e0b24f1668418a
stainii <stijnhooft@hotmail.com>
2024-03-24 13:50:33 +0100

    Remove sudo for training env

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr 10.5.0.0/16
 mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
