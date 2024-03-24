67f6543a61d1514f8cbb22dd6fef3f608eddc46c
stainii <stijnhooft@hotmail.com>
2024-03-24 18:44:24 +0100

    Fix init

--- init.sh
+++ init.sh
@@ -2,3 +2,4 @@ kubeadm init --apiserver-advertise-address $(hostname -i) --pod-network-cidr 10.
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl taint nodes --all node-role.kubernetes.io/control-plane-
